6af9f1a811e3ea841f8ba8a1244da51bbe171c7e
MHY Express <mcyalda@yahoo.com>
2018-09-23 13:01:15 -0400

    Clear the Codes.

--- HwlloWorld/Form1.cs
+++ HwlloWorld/Form1.cs
@@ -19,17 +19,14 @@ namespace HwlloWorld
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
-
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show($"Hello {textBox1.Text.ToUpper()}");
         }
     }
 }
